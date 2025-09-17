7aa5ca3910dc4b45ee6a66475a5eec864a2e639a
epicmetalcave <emillykkegaardhansen@gmail.com>
2025-09-17 11:29:49 +0200

    Save

---
+++ shell/server.js
@@ -0,0 +1,39 @@
+// shell/server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+    
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    
+    fs.readFile(path.join(__dirname, filePath), (error, content) => {
+        if (error) {
+            res.writeHead(404);
+            res.end('File not found: ' + filePath);
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
